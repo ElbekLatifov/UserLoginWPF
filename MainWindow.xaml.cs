0af6dc35d31bc12cec086f91ee1ef4bdc64feac1
ElbekLatifov <latifovelbek02@gmail.com>
2023-08-06 22:11:41 +0500

    add_timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using NewNewProject.Pages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NewNewProject
 {
@@ -21,10 +10,38 @@ namespace NewNewProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int second = 0;
+        private int hour = 0;
+        private int minut = 0;
         public MainWindow()
         {
             InitializeComponent();
             mainframe.Navigate(new MainMenuPage());
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval += TimeSpan.FromSeconds(1);
+            timer.Tick += DateShower;
+            timer.Start();
+            hour_lbl.Content = 0;
+            minut_lbl.Content = 0;
+            second_lbl.Content = 0;
+        }
+
+        private void DateShower(object sender, EventArgs e)
+        {
+            second++;
+            if(second == 60)
+            {
+                second = 0;
+                minut++;
+            }
+            if(minut == 60)
+            {
+                minut = 0;
+                hour ++;
+            }
+            hour_lbl.Content = hour;
+            minut_lbl.Content = minut;
+            second_lbl.Content = second;
         }
 
         private void Page1_Click(object sender, RoutedEventArgs e)
